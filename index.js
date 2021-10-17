d39a48eb68333abe6b4f0d3d019f9d49f78baa92
sirafr <andres22f@hotmail.com>
2021-10-16 21:01:13 -0500

    Class 3: Primer servidor

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("my app");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/',(req,res)=>{
+  res.send('Hola es mi primer server en express')
+});
+
+app.listen(port, () => {
+  console.log('Mi puerto: '+port)
+})
