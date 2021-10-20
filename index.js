5f7eb72d5865fecbeba6a27c6a4fffa73e2d5310
sirafr <andres22f@hotmail.com>
2021-10-19 23:25:14 -0500

    class 4

--- index.js
+++ index.js
@@ -11,36 +11,20 @@ app.get('/nueva-ruta',(req,res)=>{
 });
 
 app.get('/products',(req,res)=>{
-  res.json([
-    {
-      name:'Producto 1',
-      price: 1000
-    },
-    {
-      name:'Producto 2',
-      price:2000
-    }
-  ]);
-});
-
-// Agregamos un endpoint con un id
-app.get('/products/:id',(req,res)=>{
-  const { id }= req.params;
   res.json({
-      id,
-      name:'Producto 2',
-      price:2000
-    });
-});
+    name:'Producto',
+    price:'1000',
 
-// Con 2 parametros en el endpoint
-app.get('/categories/:categoryId/products/:productId',(req,res)=>{
-  const {categoryId, productId} = req.params;
-  res.json({
-    categoryId,
-    productId,
   });
-})
+});
+
+app.get('/categories',(req,res)=>{
+  res.json({
+    categories : ['drama','anime','love']
+
+
+  });
+});
 
 
 app.listen(port, () => {
