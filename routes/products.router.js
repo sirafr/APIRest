4061a188062ae213c027bd54b55681ae44a1dc7f
sirafr <andres22f@hotmail.com>
2021-10-27 23:26:32 -0500

    Class 12: Status Code

--- routes/products.router.js
+++ routes/products.router.js
@@ -27,16 +27,22 @@ router.get('/filter',(req,res)=>{
 
 router.get('/:id',(req,res)=>{
   const { id }= req.params;
-  res.json({
+  if(id==='999'){
+    res.status(404).json({
+      message:'not found'
+    });
+  } else{
+    res.status(200).json({
       id,
       name:'Producto 2',
       price:2000
     });
+  }
 });
 
 router.post('/',(req,res)=>{
   const body = req.body;
-  res.json({
+  res.status(201).json({
     message: 'created',
     data:body
   });
